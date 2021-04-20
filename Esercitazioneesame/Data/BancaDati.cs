a2daf48f0423b9563a8b1a9f19f28eb1898194e0
Tom <tomm92@outlook.it>
2021-04-20 12:40:18 +0200

    consegna esercizio

---
+++ Esercitazioneesame/Data/BancaDati.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esercitazioneesame.Data
+{
+	public class BancaDati : DbContext
+	{
+		public DbSet<Reperto> reperti { get; set; }
+
+		public BancaDati(DbContextOptions options) : base(options) { }
+	}
+}
---
+++ Esercitazioneesame/Data/Reperto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esercitazioneesame.Data
+{
+    public class Reperto
+    {
+        public int id { get; set; }
+        public string codice_reperto { get; set; }
+        public string periodo { get; set; }
+        public string tipologia { get; set; }
+        public string descrizione { get; set; }
+    }
+}
